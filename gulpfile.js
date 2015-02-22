9078e1cfebe5df2b99ef1549e187a31dd8e9c8c6
Will Franzen <wtfranzen@gmail.com>
2015-02-22 10:49:29 -0600

    Add support for sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,14 @@ var options = {
     experimental: true // Dat ES7 goodness
 };
 
+var sourcemaps = require('gulp-sourcemaps');
+
 gulp.task('compile', function() {
     gulp
         .src('src/*.js')
+        .pipe(sourcemaps.init())
         .pipe(babel(options))
+        .pipe(sourcemaps.write('maps/'))
         .pipe(gulp.dest('dist/'));
 });
 
