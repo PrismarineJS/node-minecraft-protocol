9295bcfbdb96f871713386efca416e9f73b55016
roblabla <robinlambertz+dev@gmail.com>
2015-05-21 20:22:20 +0000

    Update babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 
+var plumber = require('gulp-plumber');
 var babel = require('gulp-babel');
 var options = {
-  experimental: true // Dat ES7 goodness
+  stage: 0, // Dat ES7 goodness
+  optional: ["runtime"]
 };
 
 var sourcemaps = require('gulp-sourcemaps');
@@ -10,8 +12,15 @@ var sourcemaps = require('gulp-sourcemaps');
 gulp.task('compile', function() {
   gulp
     .src('src/**/*.js')
+    .pipe(plumber({
+      errorHandler: function(err) {
+        console.error(err.stack);
+        this.emit('end');
+      }
+    }))
     .pipe(sourcemaps.init())
     .pipe(babel(options))
+    .pipe(plumber.stop())
     .pipe(sourcemaps.write('maps/'))
     .pipe(gulp.dest('dist/'));
 });
