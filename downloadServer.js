a2bb6c1d39baa9edbba9afcbda47edb349b5a55a
Romain Beaumont <romain.rom1@gmail.com>
2015-08-17 20:08:40 +0200

    check the status code when downloading the vanilla server

--- downloadServer.js
+++ downloadServer.js
@@ -2,7 +2,11 @@ var https = require('https');
 var fs = require('fs');
 var version = require("./src/version");
 
-var file = fs.createWriteStream(process.argv[2]);
 var request = https.get("https://s3.amazonaws.com/Minecraft.Download/versions/"+version.minecraftVersion+"/minecraft_server."+version.minecraftVersion+".jar", function(response) {
-  response.pipe(file);
+  if(response.statusCode==200) {
+    var file = fs.createWriteStream(process.argv[2]);
+    response.pipe(file);
+  }
+  else
+    process.exit(1);
 });
