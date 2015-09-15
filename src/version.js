a24b79608b74574c683361c72d60717cfbfc22b4
Romain Beaumont <romain.rom1@gmail.com>
2015-09-15 15:34:38 +0200

    use minecraft-data version

--- src/version.js
+++ src/version.js
@@ -1,5 +1,3 @@
-module.exports={
-  version:47,
-  minecraftVersion:'1.8.1',
-  majorVersion:'1.8'
-};
+var majorVersion='1.8';
+var mcData=require("minecraft-data")(majorVersion);
+module.exports=mcData.version;
