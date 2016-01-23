bf04af23f9ae8a841680c99f39b4e63da10dcaff
deathcap <deathcap@gmx.co.uk>
2016-01-23 12:03:28 -0800

    client_forge example: enable forge

--- examples/client_forge/client_forge.js
+++ examples/client_forge/client_forge.js
@@ -6,6 +6,7 @@ if(process.argv.length < 4 || process.argv.length > 6) {
 }
 
 var client = mc.createClient({
+  forge: true,
   host: process.argv[2],
   port: parseInt(process.argv[3]),
   username: process.argv[4] ? process.argv[4] : "echo",
