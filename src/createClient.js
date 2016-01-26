fe327c85e132701f9f19a50fb2c92d67fb1b6b31
deathcap <deathcap@gmx.co.uk>
2016-01-25 23:57:10 -0800

    Move net.socket() and dns.resolveSrv() into client/net_tcp
    This has been the trickest to refactor/remodularize, since it used to
    modify the Client prototype to augument connect(host, port). This added
    a dependence on TCP/IP host/port socketpairs, refactor to add a
    'connect' option instead, called with the client instance. Update ping
    accordingly, but it has to call connect itself since it does not include
    client/caseCorrect, which normally calls connect. Still may be able to
    improve further and untangle these interdependencies cleanlier.

--- src/createClient.js
+++ src/createClient.js
@@ -1,5 +1,3 @@
-var net = require('net');
-var dns = require('dns');
 var Client = require('./client');
 var assert = require('assert');
 
@@ -9,28 +7,12 @@ var compress = require('./client/compress');
 var caseCorrect = require('./client/caseCorrect');
 var setProtocol = require('./client/setProtocol');
 var play = require('./client/play');
+var tcp_dns = require('./client/tcp_dns');
 
 module.exports=createClient;
 
 function createClient(options) {
   assert.ok(options, "options is required");
-  options.port = options.port || 25565;
-  options.host = options.host || 'localhost';
-
-  options.connect = (client) => {
-    if(options.port == 25565 && net.isIP(host) === 0) {
-      dns.resolveSrv("_minecraft._tcp." + options.host, function(err, addresses) {
-        if(addresses && addresses.length > 0) {
-          client.setSocket(net.connect(addresses[0].port, addresses[0].name));
-        } else {
-          client.setSocket(net.connect(options.port, options.host));
-        }
-      });
-    } else {
-      client.setSocket(net.connect(options.port, options.host));
-    }
-  };
-
   assert.ok(options.username, "username is required");
 
   var optVersion = options.version || require("./version").defaultVersion;
@@ -41,6 +23,7 @@ function createClient(options) {
 
   var client = new Client(false, options.majorVersion);
 
+  tcp_dns(client, options);
   setProtocol(client, options);
   keepalive(client, options);
   encrypt(client);
