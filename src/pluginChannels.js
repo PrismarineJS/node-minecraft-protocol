02c00cc4d409ffde2b13e02ba6d019fef25158e6
deathcap <deathcap@gmx.co.uk>
2016-01-28 21:58:15 -0800

    Duplex plugin channel stream, write side

--- src/pluginChannels.js
+++ src/pluginChannels.js
@@ -1,36 +1,39 @@
-var EventEmitter = require('events').EventEmitter;
-
-class PluginChannels extends EventEmitter {
-  constructor(client) {
-    super();
-    this.client = client;
-  }
-}
-
-class IncomingPluginChannels extends PluginChannels {
-  constructor(client) {
-    super(client);
-
-    client.on('custom_payload', (packet) => {
-      client.incomingPluginChannels.emit(packet.channel, packet.data);
-    });
-  }
-}
-
-class OutgoingPluginChannels extends PluginChannels {
-  constructor(client) {
-    super(client);
-  }
-
-  write(channel, data) {
-    this.client.write('custom_payload', {
-      channel: channel,
-      data: data
-    });
-  }
-}
+var Duplex = require('readable-stream').Duplex;
 
 module.exports = function(client, options) {
-  client.incomingPluginChannels = new IncomingPluginChannels(client);
-  client.outgoingPluginChannels = new OutgoingPluginChannels(client);
+  let channelCache = new Set();
+
+  class PluginChannelStream extends Duplex {
+    constructor(name) {
+      super();
+      this.name = name;
+    }
+
+    _write(chunk, encoding, cb) {
+      client.write('custom_payload', {
+        channel: name,
+        data: data
+      });
+      cb(null); // sync
+    }
+
+    _read() {
+      //TODO
+      /*
+      client.on('custom_payload', (packet) => {
+      client.incomingPluginChannels.emit(packet.channel, packet.data);
+      */
+    }
+  }
+
+
+  client.pluginChannel = (name) => {
+    if (channelCache[name]) {
+      return channelCache[name];
+    }
+
+    let channel = new PluginChannelStream();
+    channelCache[name] = channel;
+    return channel;
+  };
 };
