1e74dc2c1bab3aa64996c70900dbaf84e2ee7f29
Romain Beaumont <romain.rom1@gmail.com>
2017-03-12 13:51:07 +0100

    fix version checking for lower versions

--- src/client/versionChecking.js
+++ src/client/versionChecking.js
@@ -2,7 +2,9 @@ module.exports = function(client,options) {
   client.on("disconnect",message => {
     if(!message.reason)
       return;
-    const versionRequired=/Outdated client! Please use (.+)/.exec(JSON.parse(message.reason).text);
+    const parsed=JSON.parse(message.reason);
+    const text=parsed.text ? parsed.text : parsed;
+    const versionRequired=/(?:Outdated client! Please use|Outdated server! I'm still on) (.+)/.exec(text);
     if(!versionRequired)
       return;
     client.emit("error",new Error("This server is version "+versionRequired[1]+
