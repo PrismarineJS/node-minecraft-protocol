1e3dd22eba7726717bafc8ec3b6b3a23c26acadd
Romain Beaumont <romain.rom1@gmail.com>
2017-05-05 15:05:33 +0200

    fixing life

--- test/common/parallel.js
+++ test/common/parallel.js
@@ -1,5 +1,5 @@
-const nodeIndex=process.env.CIRCLE_NODE_INDEX;
-const nodeTotal=process.env.CIRCLE_NODE_TOTAL;
+const nodeIndex=parseInt(process.env.CIRCLE_NODE_INDEX);
+const nodeTotal=parseInt(process.env.CIRCLE_NODE_TOTAL);
 const parallel=nodeIndex && nodeTotal;
 const mc = require("../../");
 
@@ -10,7 +10,7 @@ const mc = require("../../");
 // (2,4,10) -> (6,8)
 // (3,4,10) -> (9,9)
 function testedRange(nodeIndex,nodeTotal,numberOfVersions) {
-  const nbFirsts=Math.ceil(numberOfVersions/nodeTotal);
+  const nbFirsts=Math.floor(numberOfVersions/nodeTotal);
   if(nodeIndex==(nodeTotal-1))
     return {firstVersion:nbFirsts*nodeIndex,lastVersion:numberOfVersions-1};
 
