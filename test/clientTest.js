93a3836ce5ed566019abb8653c440dd2a2199041
Romain Beaumont <romain.rom1@gmail.com>
2017-05-05 14:31:12 +0200

    so debug, very test

--- test/clientTest.js
+++ test/clientTest.js
@@ -16,6 +16,8 @@ if(parallel) {
   versionsByNode=Math.floor(mc.supportedVersions.length/nodeTotal);
   firstVersion=nodeIndex*versionsByNode;
   lastVersion=(nodeIndex+1)*versionsByNode-1+(nodeIndex==(nodeTotal-1) ? (mc.supportedVersions.length-versionsByNode*nodeTotal) : 0);
+  console.log("nodeIndex",nodeIndex);
+  console.log("nodeTotal",nodeTotal);
   console.log("firstVersion",firstVersion);
   console.log("lastVersion",lastVersion);
   console.log("versionsByNode",versionsByNode);
