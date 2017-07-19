6279ae9afe6679d23d02a9cf1160e89caac3e0a7
Romain Beaumont <romain.rom1@gmail.com>
2017-07-19 17:24:51 +0200

    fix socks proxy using a socks module that actually works and provide a proper node stream

--- examples/client_socks_proxy/client_socks_proxy.js
+++ examples/client_socks_proxy/client_socks_proxy.js
@@ -1,5 +1,5 @@
 const mc = require('minecraft-protocol');
-const Socks  = require("socks5-client");
+const socks  = require("socks");
 
 if(process.argv.length < 6 || process.argv.length > 8) {
   console.log("Usage : node client_socks_proxy.js <host> <port> <proxyHost> <proxyPort> [<name>] [<password>]");
@@ -9,32 +9,44 @@ if(process.argv.length < 6 || process.argv.length > 8) {
 const proxyHost=process.argv[4];
 const proxyPort=process.argv[5];
 
-const client = mc.createClient({
-  stream: Socks.createConnection({
+socks.createConnection({
+  proxy: {
+    ipaddress: proxyHost,
+    port: proxyPort,
+    type: 5
+  },
+  target: {
     host: process.argv[2],
-    port: parseInt(process.argv[3]),
-    socksHost: proxyHost,
-    socksPort: proxyPort
-  }),
-  username: process.argv[6] ? process.argv[6] : "echo",
-  password: process.argv[7]
+    port: parseInt(process.argv[3])
+  },
+}, function(err, socket) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const client = mc.createClient({
+    stream: socket,
+    username: process.argv[6] ? process.argv[6] : "echo",
+    password: process.argv[7]
+  });
+
+  client.on('connect', function() {
+    console.info('connected');
+  });
+  client.on('disconnect', function(packet) {
+    console.log('disconnected: '+ packet.reason);
+  });
+  client.on('end', function(err) {
+    console.log('Connection lost');
+  });
+  client.on('chat', function(packet) {
+    const jsonMsg = JSON.parse(packet.message);
+    if(jsonMsg.translate === 'chat.type.announcement' || jsonMsg.translate === 'chat.type.text') {
+      const username = jsonMsg.with[0].text;
+      const msg = jsonMsg.with[1];
+      if(username === client.username) return;
+      client.write('chat', {message: msg});
+    }
+  });
 });
 
-client.on('connect', function() {
-  console.info('connected');
-});
-client.on('disconnect', function(packet) {
-  console.log('disconnected: '+ packet.reason);
-});
-client.on('end', function(err) {
-  console.log('Connection lost');
-});
-client.on('chat', function(packet) {
-  const jsonMsg = JSON.parse(packet.message);
-  if(jsonMsg.translate === 'chat.type.announcement' || jsonMsg.translate === 'chat.type.text') {
-    const username = jsonMsg.with[0].text;
-    const msg = jsonMsg.with[1];
-    if(username === client.username) return;
-    client.write('chat', {message: msg});
-  }
-});
