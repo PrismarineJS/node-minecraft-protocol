70a679d383280c674a610fe7d9b846e11b75bc8d
plexigras <plexigras@aol.com>
2017-11-01 16:56:52 +0100

    added buffer arg to packet events

--- src/client.js
+++ src/client.js
@@ -98,8 +98,8 @@ class Client extends EventEmitter
       parsed.metadata.state=state;
       debug("read packet " + state + "." + parsed.metadata.name);
       debug(parsed.data);
-      this.emit('packet', parsed.data, parsed.metadata);
-      this.emit(parsed.metadata.name, parsed.data, parsed.metadata);
+      this.emit('packet', parsed.data, parsed.metadata, parsed.buffer);
+      this.emit(parsed.metadata.name, parsed.data, parsed.metadata, parsed.buffer);
       this.emit('raw.' + parsed.metadata.name, parsed.buffer, parsed.metadata);
       this.emit('raw', parsed.buffer, parsed.metadata);
     });
