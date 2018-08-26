966c15f8ab341cc544cbe6bd8170515c6b80dfaa
Romain Beaumont <romain.rom1@gmail.com>
2018-08-26 16:01:28 +0200

    Make tested version 1.13.1 instead of 1.13

--- src/version.js
+++ src/version.js
@@ -2,5 +2,5 @@
 
 module.exports = {
   defaultVersion: '1.12.2',
-  supportedVersions: ['1.7', '1.8', '1.9', '1.10', '1.11.2', '1.12.2', '1.13']
+  supportedVersions: ['1.7', '1.8', '1.9', '1.10', '1.11.2', '1.12.2', '1.13.1']
 }
