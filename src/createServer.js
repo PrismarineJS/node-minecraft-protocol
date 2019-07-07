d3a9f7dcea376feade7904b0529f579275f3f5df
Romain Beaumont <romain.rom1@gmail.com>
2019-07-07 23:26:28 +0200

    throw an unsupported protocol version in createServer when asking for an unsupported version

--- src/createServer.js
+++ src/createServer.js
@@ -26,6 +26,7 @@ function createServer (options = {}) {
   const optVersion = version === undefined || version === false ? require('./version').defaultVersion : version
 
   const mcData = require('minecraft-data')(optVersion)
+  if (!mcData) throw new Error(`unsupported protocol version: ${optVersion}`)
   const mcversion = mcData.version
   const hideErrors = options.hideErrors || false
 
