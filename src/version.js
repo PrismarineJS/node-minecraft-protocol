b3faa540864f4c1641c15d58988ff155e81a16bf
Romain Beaumont <romain.rom1@gmail.com>
2019-07-28 21:45:20 +0200

    Make 1.14.4 the tested version of 1.14 major

--- src/version.js
+++ src/version.js
@@ -1,6 +1,6 @@
 'use strict'
 
 module.exports = {
-  defaultVersion: '1.14.3',
-  supportedVersions: ['1.7', '1.8', '1.9', '1.10', '1.11.2', '1.12.2', '1.13.2', '1.14.3']
+  defaultVersion: '1.14.4',
+  supportedVersions: ['1.7', '1.8', '1.9', '1.10', '1.11.2', '1.12.2', '1.13.2', '1.14.4']
 }
