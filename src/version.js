c0ae9b1546e8c5fc5d842393c23543289d005bf3
Romain Beaumont <romain.rom1@gmail.com>
2019-12-29 22:56:59 +0100

    update mcdata and bring 1.15.1 support

--- src/version.js
+++ src/version.js
@@ -1,6 +1,6 @@
 'use strict'
 
 module.exports = {
-  defaultVersion: '1.14.4',
-  supportedVersions: ['1.7', '1.8', '1.9', '1.10', '1.11.2', '1.12.2', '1.13.2', '1.14.4']
+  defaultVersion: '1.15.1',
+  supportedVersions: ['1.7', '1.8', '1.9', '1.10', '1.11.2', '1.12.2', '1.13.2', '1.14.4', '1.15.1']
 }
