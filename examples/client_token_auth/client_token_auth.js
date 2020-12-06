7f430b198a1da2f8ffcfb7648dcc8522570f3d5e
ph0t0shop <lankreijersten@gmail.com>
2020-12-06 16:31:46 +0100

    Fix examples

---
+++ examples/client_token_auth/client_token_auth.js
@@ -0,0 +1,28 @@
+const mc = require('minecraft-protocol')
+
+if (process.argv.length !== 5) {
+  console.log('Usage : node echo.js <host> <port> <name>')
+  process.exit(1)
+}
+
+const client = mc.createClient({
+  host: process.argv[2],
+  port: parseInt(process.argv[3]),
+  username: process.argv[4]
+})
+
+client.on('error', function (err) {
+  console.error(err)
+})
+client.on('session', function (session) {
+  console.info('session initialized ', JSON.stringify(session))
+})
+client.on('connected', function () {
+  console.info('connected')
+})
+client.on('disconnect', function (packet) {
+  console.log('disconnected: ' + packet.reason)
+})
+client.on('end', function () {
+  console.log('Connection lost')
+})
