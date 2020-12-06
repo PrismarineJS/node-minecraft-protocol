14c4886ac4de6e3e2f7c69224f56cf408da1ef7a
Romain Beaumont <romain.rom1@gmail.com>
2020-12-06 01:57:40 +0100

    Revert "Add token authentication (#780)"
    This reverts commit 0277091ddc1efe04f9beeb19c63147b390047caa.
    This commits introduced a bug in offline mode.
    We don't know yet how to fix it, so better revert first and fix later

--- examples/client_echo/client_echo.js
+++ examples/client_echo/client_echo.js
@@ -1,17 +1,15 @@
 const mc = require('minecraft-protocol')
 
-if (process.argv.length !== 4) {
-  console.log('Usage : node echo.js <host> <port> [<name>]')
+if (process.argv.length < 4 || process.argv.length > 6) {
+  console.log('Usage : node echo.js <host> <port> [<name>] [<password>]')
   process.exit(1)
 }
 
 const client = mc.createClient({
   host: process.argv[2],
   port: parseInt(process.argv[3]),
-  username: process.argv[4] ? process.argv[4] : 'echo'
-})
-client.on('error', function (err) {
-  console.error(err)
+  username: process.argv[4] ? process.argv[4] : 'echo',
+  password: process.argv[5]
 })
 
 client.on('connect', function () {
