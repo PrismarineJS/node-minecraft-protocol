5d723e9a04f07504c1774916d6df8f4573b39fd6
Jordan Jones <Kashalls@users.noreply.github.com>
2020-12-11 12:30:48 -0800

    Integrate Authentication for Microsoft Accounts (#788)
    * add node-fetch and @xboxreplay/xboxlive-auth for microsoft/xbox auth
    
    * decide which authentication to use based on options;
    
    if options.auth === 'microsoft' then use microsoft/xbox auth, else use Yggdrasil until they kill that.
    
    * push working auth
    
    * commentary
    
    * eslint does not like me :(
    
    * User-Agent works just fine without version
    
    * linting = 95% of development
    
    * revert changes to encrypt.js
    
    * set haveCredentials to whether or not we have a token.
    
    Technically this will always be true so...?
    
    * eslint
    
    * mod+create: api + example
    
    * mod: readme.md

---
+++ examples/client_microsoft_auth/client_microsoft_auth.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const mc = require('minecraft-protocol')
+
+if (process.argv.length < 4 || process.argv.length > 6) {
+  console.log('Usage : node echo.js <host> <port> [<email>] [<password>]')
+  process.exit(1)
+}
+
+const client = mc.createClient({
+  host: process.argv[2],
+  port: parseInt(process.argv[3]),
+  username: process.argv[4], // your microsoft account email
+  password: process.argv[5], // your microsoft account password
+  auth: 'microsoft' // This option must be present and set to 'microsoft' to use Microsoft Account Authentication. Failure to do so will result in yggdrasil throwing invalid account information.
+})
+
+client.on('connect', function () {
+  console.info('connected')
+})
+client.on('disconnect', function (packet) {
+  console.log('disconnected: ' + packet.reason)
+})
+client.on('chat', function (packet) {
+  const jsonMsg = JSON.parse(packet.message)
+  if (jsonMsg.translate === 'chat.type.announcement' || jsonMsg.translate === 'chat.type.text') {
+    const username = jsonMsg.with[0].text
+    const msg = jsonMsg.with[1]
+    if (username === client.username) return
+    client.write('chat', { message: msg })
+  }
+})
