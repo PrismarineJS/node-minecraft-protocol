8f2a027812931d0bef48ed1da2a50a16878dc536
extremeheat <extreme@protonmail.ch>
2021-01-29 19:21:03 -0500

    Msa device code auth (#806)
    * initial msa work
    
    * rm debug code
    
    * Update package.json
    
    * lint, seperate constants, create missing msa cache
    
    * support multiple profiles
    
    * lint
    
    * use shared constants
    
    * fix path issues
    
    * fix token variable
    
    * fix caching msa profile data
    
    * switch clientId to one from microsoft
    
    * store caches in .minecraft, fallback to dev code auth when user+pass fails
    
    * update electron demo, fix error handling, add docs
    
    * fix caching dir
    
    * fix lint
    
    * move to class scope, token fixes
    
    * retry on fail, terminology fixes
    
    * fix promise bug
    
    * cleanup

--- docs/API.md
+++ docs/API.md
@@ -74,11 +74,16 @@ Returns a `Client` instance and perform login.
 `options` is an object containing the properties :
  * username
  * port : default to 25565
- * password : can be omitted (if the tokens and profilesFolder are also omitted then it tries to connect in offline mode)
+ * auth : the type of account to use, either `microsoft` or `mojang`. default to 'mojang'
+ * password : can be omitted
+   * (microsoft account) leave this blank to use device code auth. If you provide
+   a password, we try to do username and password auth, but this does not always work.
+   * (mojang account) If provided, we auth with the username and password. If this
+   is blank, and `profilesFolder` is specified, we auth with the tokens there instead.
+   If neither `password` or `profilesFolder` are specified, we connect in offline mode.
  * host : default to localhost
  * clientToken : generated if a password is given
  * accessToken : generated if a password or microsoft account is given
- * auth : the type of auth server to use, either 'microsoft' or 'mojang'. default to 'mojang'
  * authServer : auth server, default to https://authserver.mojang.com
  * sessionServer : session server, default to https://sessionserver.mojang.com
  * keepAlive : send keep alive packets : default to true
@@ -93,7 +98,12 @@ Returns a `Client` instance and perform login.
  * connect : a function taking the client as parameter and that should client.setSocket(socket) 
  and client.emit('connect') when appropriate (see the proxy examples for an example of use)
  * agent : a http agent that can be used to set proxy settings for yggdrasil authentication (see proxy-agent on npm) 
- * profilesFolder : the path to the folder that contains your `launcher_profiles.json`. defaults to your minecraft folder if it exists, otherwise the local directory. set to `false` to disable managing profiles
+ * profilesFolder : optional
+   * (mojang account) the path to the folder that contains your `launcher_profiles.json`. defaults to your minecraft folder if it exists, otherwise the local directory. set to `false` to disable managing profiles 
+   * (microsoft account) the path to store authentication caches, defaults to .minecraft
+ * onMsaCode(data) : (optional) callback called when signing in with a microsoft account
+ with device code auth. `data` is an object documented [here](https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-device-code#device-authorization-response)
+
 
 ## mc.Client(isServer,version,[customPackets])
 
--- src/client/microsoftAuth.js
+++ src/client/microsoftAuth.js
@@ -1,10 +1,8 @@
 const XboxLiveAuth = require('@xboxreplay/xboxlive-auth')
+const debug = require('debug')('minecraft-protocol')
 const fetch = require('node-fetch')
-
-const XSTSRelyingParty = 'rp://api.minecraftservices.com/'
-const MinecraftServicesLogWithXbox = 'https://api.minecraftservices.com/authentication/login_with_xbox'
-const MinecraftServicesEntitlement = 'https://api.minecraftservices.com/entitlements/mcstore'
-const MinecraftServicesProfile = 'https://api.minecraftservices.com/minecraft/profile'
+const authConstants = require('./authConstants')
+const { MsAuthFlow } = require('./authFlow.js')
 
 const getFetchOptions = {
   headers: {
@@ -14,53 +12,110 @@ const getFetchOptions = {
 }
 
 /**
- * Authenticates with Xbox Live, then Authenticates with Minecraft, Checks Entitlements and Gets Profile.
- * @function
+ * Obtains Minecaft profile data using a Minecraft access token and starts the join sequence
  * @param {object} client - The client passed to protocol
  * @param {object} options - Client Options
+ * @param {string} mcAccessToken - Minecraft access token for session server
+ * @param {object?} msa - Cached Microsoft account data for more descriptive errors
  */
+async function postAuthenticate (client, options, mcAccessToken, msa) {
+  options.haveCredentials = mcAccessToken != null
 
-module.exports = async (client, options) => {
-  // Use external library to authenticate with
-  const XAuthResponse = await XboxLiveAuth.authenticate(options.username, options.password, { XSTSRelyingParty })
-    .catch((err) => {
-      if (err.details) throw new Error(`Unable to authenticate with Xbox Live: ${JSON.stringify(err.details)}`)
-      else throw Error(err)
-    })
+  let minecraftProfile
+  const res = await fetch(authConstants.MinecraftServicesProfile, getFetchOptions)
+  if (res.ok) { // res.status >= 200 && res.status < 300
+    minecraftProfile = await res.json()
+  } else {
+    const user = msa ? msa.getUsers()[0] : options.username
+    throw Error(`Failed to obtain Minecraft profile data for '${user?.username}', does the account own Minecraft Java? Server returned: ${res.statusText}`)
+  }
 
-  const MineServicesResponse = await fetch(MinecraftServicesLogWithXbox, {
-    method: 'post',
-    ...getFetchOptions,
-    body: JSON.stringify({ identityToken: `XBL3.0 x=${XAuthResponse.userHash};${XAuthResponse.XSTSToken}` })
-  }).then(checkStatus)
-
-  options.haveCredentials = MineServicesResponse.access_token != null
-
-  getFetchOptions.headers.Authorization = `Bearer ${MineServicesResponse.access_token}`
-  const MineEntitlements = await fetch(MinecraftServicesEntitlement, getFetchOptions).then(checkStatus)
-  if (MineEntitlements.items.length === 0) throw Error('This user does not have any items on its accounts according to minecraft services.')
-
-  const MinecraftProfile = await fetch(MinecraftServicesProfile, getFetchOptions).then(checkStatus)
-  if (!MinecraftProfile.id) throw Error('This user does not own minecraft according to minecraft services.')
+  if (!minecraftProfile.id) {
+    debug('[mc] profile', minecraftProfile)
+    throw Error('This user does not own minecraft according to minecraft services.')
+  }
 
   // This profile / session here could be simplified down to where it just passes the uuid of the player to encrypt.js
   // That way you could remove some lines of code. It accesses client.session.selectedProfile.id so /shrug.
   // - Kashalls
   const profile = {
-    name: MinecraftProfile.name,
-    id: MinecraftProfile.id
+    name: minecraftProfile.name,
+    id: minecraftProfile.id
   }
 
   const session = {
-    accessToken: MineServicesResponse.access_token,
+    accessToken: mcAccessToken,
     selectedProfile: profile,
     availableProfile: [profile]
   }
   client.session = session
-  client.username = MinecraftProfile.name
-  options.accessToken = MineServicesResponse.access_token
+  client.username = minecraftProfile.name
+  options.accessToken = mcAccessToken
   client.emit('session', session)
   options.connect(client)
+}
+
+/**
+ * Authenticates with Mincrosoft through user credentials, then
+ * with Xbox Live, Minecraft, checks entitlements and returns profile
+ *
+ * @function
+ * @param {object} client - The client passed to protocol
+ * @param {object} options - Client Options
+ */
+async function authenticatePassword (client, options) {
+  let XAuthResponse
+
+  try {
+    XAuthResponse = await XboxLiveAuth.authenticate(options.username, options.password, { XSTSRelyingParty: authConstants.XSTSRelyingParty })
+      .catch((err) => {
+        console.warn('Unable to authenticate with Microsoft', err)
+        throw err
+      })
+  } catch (e) {
+    console.info('Retrying auth with device code flow')
+    return await authenticateDeviceCode(client, options)
+  }
+
+  try {
+    const MineServicesResponse = await fetch(authConstants.MinecraftServicesLogWithXbox, {
+      method: 'post',
+      ...getFetchOptions,
+      body: JSON.stringify({ identityToken: `XBL3.0 x=${XAuthResponse.userHash};${XAuthResponse.XSTSToken}` })
+    }).then(checkStatus)
+
+    getFetchOptions.headers.Authorization = `Bearer ${MineServicesResponse.access_token}`
+    const MineEntitlements = await fetch(authConstants.MinecraftServicesEntitlement, getFetchOptions).then(checkStatus)
+    if (MineEntitlements.items.length === 0) throw Error('This user does not have any items on its accounts according to minecraft services.')
+
+    await postAuthenticate(client, options, MineServicesResponse.access_token)
+  } catch (err) {
+    console.error(err)
+    client.emit('error', err)
+  }
+}
+
+/**
+ * Authenticates to Minecraft via device code based Microsoft auth,
+ * then connects to the specified server in Client Options
+ *
+ * @function
+ * @param {object} client - The client passed to protocol
+ * @param {object} options - Client Options
+ */
+async function authenticateDeviceCode (client, options) {
+  try {
+    const flow = new MsAuthFlow(options.username, options.profilesFolder, options.onMsaCode)
+
+    const token = await flow.getMinecraftToken()
+    debug('Acquired Minecraft token', token.slice(0, 16))
+
+    getFetchOptions.headers.Authorization = `Bearer ${token}`
+    await postAuthenticate(client, options, token, flow.msa)
+  } catch (err) {
+    console.error(err)
+    client.emit('error', err)
+  }
 }
 
 function checkStatus (res) {
@@ -69,4 +124,19 @@ function checkStatus (res) {
   } else {
     throw Error(res.statusText)
   }
+}
+
+module.exports = {
+  authenticatePassword,
+  authenticateDeviceCode
+}
+
+async function msaTest () {
+  // MsAuthFlow.resetTokenCaches()
+  await authenticateDeviceCode({ emit: () => { } }, {})
+}
+
+// debug with node microsoftAuth.js
+if (!module.parent) {
+  msaTest()
 }
