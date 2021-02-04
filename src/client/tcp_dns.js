56d9ae8444cfe5ffe1b29a228b1613be92e1e090
u9g <43508353+u9g@users.noreply.github.com>
2021-02-04 14:49:30 -0500

    Refactored tcp_dns and made it work again
    previously was failing in some cases for users trying to join certain servers

--- src/client/tcp_dns.js
+++ src/client/tcp_dns.js
@@ -1,37 +1,54 @@
 const net = require('net')
 const dns = require('dns')
+const util = require('util')
+const resolveSrv = util.promisify(dns.resolveSrv)
 
-const debug = require('debug')('minecraft-protocol')
-
-module.exports = function (client, options) {
+module.exports = (client, options) => {
   // Default options
   options.port = options.port || 25565
   options.host = options.host || 'localhost'
+  if (options.connect) return
+  options.connect = async (passedClient) => connect(passedClient, options)
+}
 
-  if (!options.connect) {
-    options.connect = (client) => {
-      // Use stream if provided
-      if (options.stream) {
-        client.setSocket(options.stream)
-        return client.emit('connect')
-      }
-
-      // If port was not defined (defaults to 25565), host is not an ip and is not localhost
-      if (options.port === 25565 && net.isIP(options.host) === 0 && options.host !== 'localhost') {
-        // Try to resolve SRV records for the comain
-        dns.resolveSrv(`_minecraft._tcp.${options.host}`, (err, addresses) => {
-          // Error resolving domain
-          if (err) debug(`srv lookup failed: ${err}`)
-
-          // SRV Lookup resolved correctly
-          if (addresses && addresses.length > 0) {
-            debug(`srv lookup returned ${addresses}`)
-            options.host = addresses[0].name
-            options.port = addresses[0].port
-          }
-        })
-      }
-      return client.setSocket(net.connect(options.port, options.host))
-    }
+async function connect (client, options) {
+  // Use stream if provided
+  if (options.stream) {
+    client.setSocket(options.stream)
+    client.emit('connect')
+    return
   }
+
+  // If port was not defined (defauls to 25565), host is not an ip neither localhost
+  if (
+    options.port === 25565 &&
+        net.isIP(options.host) === 0 &&
+        options.host !== 'localhost'
+  ) {
+    // Try to resolve SRV records for the comain
+    try {
+      const addresses = await resolveSrv(`_minecraft._tcp.${options.host}`)
+      const [address] = addresses
+      const { name, port } = address
+      // SRV Lookup resolved conrrectly
+      if (addresses?.length > 0) {
+        options.host = name
+        options.port = port
+        client.setSocket(net.connect(port, name))
+      } else {
+        // Otherwise, just connect using the provided hostname and port
+        connectUsingOptions(client, options)
+      }
+    } catch (err) { // Error resolving domain
+      // Could not resolve SRV lookup, connect directly
+      connectUsingOptions(client, options)
+    }
+  } else {
+    // Otherwise, just connect using the provided hostname and port
+    connectUsingOptions(client, options)
+  }
+}
+
+function connectUsingOptions (client, { host, port }) {
+  client.setSocket(net.connect(port, host))
 }
