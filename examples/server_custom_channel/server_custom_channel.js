61f14b8354fadd6648b373984050f903d98576f6
Nihat <73744616+KaffinPX@users.noreply.github.com>
2021-04-05 13:52:02 +0300

    Update server_custom_channel.js

--- examples/server_custom_channel/server_custom_channel.js
+++ examples/server_custom_channel/server_custom_channel.js
@@ -7,27 +7,17 @@ const server = mc.createServer({
   port: 25565, // optional
   version: '1.16'
 })
-const mcData = require('minecraft-data')(server.version)
-const loginPacket = mcData.loginPacket
 
 server.on('login', function (client) {
-  client.write('login', {
+   client.write('login', {
     entityId: client.id,
-    isHardcore: false,
+    levelType: 'default',
     gameMode: 0,
-    previousGameMode: 255,
-    worldNames: loginPacket.worldNames,
-    dimensionCodec: loginPacket.dimensionCodec,
-    dimension: loginPacket.dimension,
-    worldName: 'minecraft:overworld',
-    hashedSeed: [0, 0],
+    dimension: 0,
+    difficulty: 2,
     maxPlayers: server.maxPlayers,
-    viewDistance: 10,
-    reducedDebugInfo: false,
-    enableRespawnScreen: true,
-    isDebug: false,
-    isFlat: false
-  })
+    reducedDebugInfo: false
+  });
   client.registerChannel('CUSTOM|ChannelOne', ['i32', []], true)
   client.registerChannel('CUSTOM|ChannelTwo', ['i32', []], true)
   client.write('position', {
