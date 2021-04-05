e5b2511ad9c70f9e33ce88b153c3343813f3c121
Nihat <73744616+KaffinPX@users.noreply.github.com>
2021-04-05 13:51:37 +0300

    Update server_channel.js

--- examples/server_channel/server_channel.js
+++ examples/server_channel/server_channel.js
@@ -7,30 +7,20 @@ const server = mc.createServer({
   port: 25565, // optional
   version: '1.16'
 })
-const mcData = require('minecraft-data')(server.version)
-const loginPacket = mcData.loginPacket
 
 server.on('login', function (client) {
   client.registerChannel('MC|Brand', ['string', []])
   client.on('MC|Brand', console.log)
 
-  client.write('login', {
+   client.write('login', {
     entityId: client.id,
-    isHardcore: false,
+    levelType: 'default',
     gameMode: 0,
-    previousGameMode: 255,
-    worldNames: loginPacket.worldNames,
-    dimensionCodec: loginPacket.dimensionCodec,
-    dimension: loginPacket.dimension,
-    worldName: 'minecraft:overworld',
-    hashedSeed: [0, 0],
+    dimension: 0,
+    difficulty: 2,
     maxPlayers: server.maxPlayers,
-    viewDistance: 10,
-    reducedDebugInfo: false,
-    enableRespawnScreen: true,
-    isDebug: false,
-    isFlat: false
-  })
+    reducedDebugInfo: false
+  });
   client.write('position', {
     x: 0,
     y: 1.62,
