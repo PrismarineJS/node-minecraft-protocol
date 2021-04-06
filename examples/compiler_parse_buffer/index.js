7ff1a9217ae7931a894063567097f64c6d7711df
u9g <43508353+u9g@users.noreply.github.com>
2021-04-06 02:13:52 -0400

    Initial commit

---
+++ examples/compiler_parse_buffer/index.js
@@ -0,0 +1,20 @@
+const { createSerializer, createDeserializer, states } = require('../../src/index.js')
+const mcPackets = require('minecraft-packets')
+
+const serializer = createSerializer({ state: states.PLAY, version: '1.16.5' })
+const deserializer = createDeserializer({ state: states.PLAY, version: '1.16.5' })
+
+function convertBufferToObject (buffer) {
+  return deserializer.parsePacketBuffer(buffer)
+}
+
+function convertObjectToBuffer (object) {
+  return serializer.createPacketBuffer(object)
+}
+
+const buffer = mcPackets.pc['1.16.5']['from-server'].abilities[0].raw
+const parsed = convertBufferToObject(buffer).data
+const parsedBuffer = convertObjectToBuffer(parsed)
+console.log(buffer)
+console.log(parsedBuffer)
+console.log(buffer === parsedBuffer)
