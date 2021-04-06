72e2f7e3e9444c8e0edc821fed3532a65ce61478
u9g <43508353+u9g@users.noreply.github.com>
2021-04-06 08:34:32 -0400

    Add packet cycletests

---
+++ test/cycleTest.js
@@ -0,0 +1,38 @@
+const { createSerializer, createDeserializer, states } = require('minecraft-protocol')
+const mcPackets = require('minecraft-packets')
+const assert = require('assert')
+
+const makeClientSerializer = version => createSerializer({ state: states.PLAY, version, isServer: true })
+const makeClientDeserializer = version => createDeserializer({ state: states.PLAY, version })
+let serializer = null
+let deserializer = null
+
+function convertBufferToObject (buffer) {
+  return deserializer.parsePacketBuffer(buffer)
+}
+
+function convertObjectToBuffer (object) {
+  return serializer.createPacketBuffer(object)
+}
+
+function testBuffer (buffer, [packetName, packetIx]) {
+  const parsed = convertBufferToObject(buffer).data
+  const parsedBuffer = convertObjectToBuffer(parsed)
+  const areEq = buffer.equals(parsedBuffer)
+  assert(areEq === true, `Error when testing ${+packetIx + 1} ${packetName} packet`)
+}
+
+Object.entries(mcPackets.pc).forEach(([ver, data]) => {
+  serializer = makeClientSerializer(ver)
+  deserializer = makeClientDeserializer(ver)
+  // server -> client
+  console.log(`${ver} tests starting now!`)
+  let counter = 0
+  Object.entries(data['from-server']).forEach(([packetName, packetData]) => {
+    for (const i in packetData) {
+      testBuffer(packetData[i].raw, [packetName, i])
+      counter++
+    }
+  })
+  console.log(`Cycled ${counter} packets for ${ver} successfully`)
+})
