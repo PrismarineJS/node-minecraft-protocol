b91afb818c135a09608f49af9edf544f218aebe2
u9g <43508353+u9g@users.noreply.github.com>
2021-04-06 21:15:56 -0400

    make tests non-parallel

--- test/non-par-test.js
+++ test/non-par-test.js
@@ -1,5 +1,5 @@
+/* eslint-env mocha */
 // Tests packet serialization/deserialization from with raw binary from minecraft-packets
-const { firstVersion, lastVersion } = require('./common/parallel')
 const testedVersions = ['1.16', '1.16.5']
 
 const { createSerializer, createDeserializer, states } = require('minecraft-protocol')
@@ -23,23 +23,24 @@ function testBuffer (buffer, [packetName, packetIx]) {
   const parsed = convertBufferToObject(buffer).data
   const parsedBuffer = convertObjectToBuffer(parsed)
   const areEq = buffer.equals(parsedBuffer)
-  assert(areEq === true, `Error when testing ${+packetIx + 1} ${packetName} packet`)
+  assert.strictEqual(areEq, true, `Error when testing ${+packetIx + 1} ${packetName} packet`)
 }
 
 testedVersions.forEach((ver, i) => {
-  if (!(i >= firstVersion && i <= lastVersion)) { return }
-
-  const data = mcPackets.pc[ver]
-  serializer = makeClientSerializer(ver)
-  deserializer = makeClientDeserializer(ver)
-  // server -> client
-  console.log(`${ver} tests starting now!`)
-  let counter = 0
-  Object.entries(data['from-server']).forEach(([packetName, packetData]) => {
-    for (const i in packetData) {
-      testBuffer(packetData[i].raw, [packetName, i])
-      counter++
-    }
+  describe(`Test version ${ver}`, () => {
+    const data = mcPackets.pc[ver]
+    serializer = makeClientSerializer(ver)
+    deserializer = makeClientDeserializer(ver)
+    // server -> client
+    let counter = 0
+    Object.entries(data['from-server']).forEach(([packetName, packetData]) => {
+      it(`${packetName} packet`, () => {
+        for (const i in packetData) {
+          testBuffer(packetData[i].raw, [packetName, i])
+          counter++
+        }
+      })
+    })
+    console.log(`Cycled ${counter} packets for ${ver} successfully`)
   })
-  console.log(`Cycled ${counter} packets for ${ver} successfully`)
 })
