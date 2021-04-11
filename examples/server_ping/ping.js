4a85609902aa9626c4b7ca63de75250abea4058e
Nihat <73744616+KaffinPX@users.noreply.github.com>
2021-04-12 00:13:06 +0300

    Update remove color regex in ping.js (#848)

--- examples/server_ping/ping.js
+++ examples/server_ping/ping.js
@@ -6,7 +6,7 @@ if (process.argv.length < 3 || process.argv.length > 3) { // Check for args for 
 }
 
 function removeColorsFromString (text) { // Removing minecraft colors from strings, because console can`t read it and it will look crazy.
-  return text.replace(/§a|§b|§c|§d|§e|§1|§2|§3|§4|§5|§6|§7|§8|§9|§k|§l|§m|§n|§o|§f|§r/g, '')
+  return text.replace(/§./g, '')
 }
 
 let host
