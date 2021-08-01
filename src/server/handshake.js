9dfafbf214cda42cdeee1be5d8ed238cd79c6ca3
Beanes <29002908+Beaness@users.noreply.github.com>
2021-08-01 21:16:44 +0200

    Simplify

--- src/server/handshake.js
+++ src/server/handshake.js
@@ -1,4 +1,3 @@
-const mcData = require('minecraft-data')
 const states = require('../states')
 
 module.exports = function (client, server, { version }) {
@@ -8,13 +7,13 @@ module.exports = function (client, server, { version }) {
     client.serverHost = packet.serverHost
     client.serverPort = packet.serverPort
     client.protocolVersion = packet.protocolVersion
+
     if (version === false || version === undefined) {
       if (client.protocolVersion === 5) { // The first snapshot versions of 1.8 uses the 1.7 protocol id, after 1.8 each snapshot has a different protocol id.
         client.version = '1.7.10'
       } else {
-        const postNettyVersions = mcData.postNettyVersionsByProtocolVersion.pc[client.protocolVersion]
-        if (postNettyVersions && postNettyVersions.length > 0) {
-          client.version = postNettyVersions[0].minecraftVersion
+        if (require('minecraft-data')(client.protocolVersion)) {
+          client.version = client.protocolVersion
         } else {
           client.end('Protocol version ' + client.protocolVersion + ' is not supported')
         }
