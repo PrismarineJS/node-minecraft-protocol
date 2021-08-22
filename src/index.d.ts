07468e904975d16ca7814087f0985f757ded14ac
u9g <43508353+u9g@users.noreply.github.com>
2021-08-21 20:59:45 -0400

    Change how we export states enum

--- src/index.d.ts
+++ src/index.d.ts
@@ -117,7 +117,7 @@ declare module 'minecraft-protocol' {
 		version: string
 	}
 
-	enum States {
+	enum states {
 		HANDSHAKING = 'handshaking',
 		LOGIN = 'login',
 		PLAY = 'play',
@@ -168,7 +168,6 @@ declare module 'minecraft-protocol' {
 		latency: number
 	}
 
-	export const states: typeof States
 	export const supportedVersions: ['1.7', '1.8', '1.9', '1.10', '1.11.2', '1.12.2', '1.13.2', '1.14.4', '1.15.2', '1.16.5', '1.17.1']
 
 	export function createServer(options: ServerOptions): Server
