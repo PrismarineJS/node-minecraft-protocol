f777f8b8f72e698a4c035b8e9b8af721130e7687
Romain Beaumont <romain.rom1@gmail.com>
2021-11-03 01:13:40 +0100

    Implement Prismarine-Auth (#884) (#917)

--- src/client/authConstants.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  XSTSRelyingParty: 'rp://api.minecraftservices.com/',
-  MinecraftServicesLogWithXbox: 'https://api.minecraftservices.com/authentication/login_with_xbox',
-  MinecraftServicesEntitlement: 'https://api.minecraftservices.com/entitlements/mcstore',
-  MinecraftServicesProfile: 'https://api.minecraftservices.com/minecraft/profile'
-}
