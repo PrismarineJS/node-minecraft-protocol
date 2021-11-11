3c821a5a9b645cadedfa5462dfb01ef033cf8550
extremeheat <extreme@protonmail.ch>
2021-11-11 14:21:50 -0500

    Update prismarine-auth usage
    add new `deviceType` parameter

--- src/client/microsoftAuth.js
+++ src/client/microsoftAuth.js
@@ -10,17 +10,22 @@ async function authenticate (client, options) {
 
   if (options.authTitle === undefined) {
     options.authTitle = Titles.MinecraftJava
+    options.deviceType = 'Win32'
   }
 
   const Authflow = new PrismarineAuth(options.username, options.profilesFolder, options, options.onMsaCode)
-  const { token, entitlements, profile } = await Authflow.getMinecraftJavaToken({ fetchEntitlements: true, fetchProfile: true })
+  const { token, entitlements, profile } = await Authflow.getMinecraftJavaToken({ fetchEntitlements: true, fetchProfile: true }).catch(e => {
+    if (options.password) console.warn('Sign in failed, try removing the password field')
+    throw e
+  })
 
-  if (entitlements.items.length === 0) throw Error('This user does not possess any entitlements on this account according to minecraft services.')
   debug('[mc] entitlements', entitlements)
+  debug('[mc] profile', profile)
+  
+  if (!entitlements.items.length) throw Error(`Signed in account ${options.username} doesn't appear to own Minecraft`)  
+  if (profile.error) throw Error(`Failed to obtain profile data for ${options.username}, does the account own minecraft?`)
 
   options.haveCredentials = token !== null
-  if (profile.error) throw Error(`Failed to obtain profile data for ${options.username}, does the account own minecraft?\n${profile}`)
-  debug('[mc] profile', profile)
 
   const session = {
     accessToken: token,
