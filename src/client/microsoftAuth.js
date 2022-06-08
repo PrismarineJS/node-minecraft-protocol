cc6b73b30068d65a9c778db3f9f5dd5d79afcd2e
LucienHH <lucienharryholloway@hotmail.co.uk>
2022-06-09 00:32:21 +0100

    Implement Realm joining

--- src/client/microsoftAuth.js
+++ src/client/microsoftAuth.js
@@ -2,17 +2,18 @@ const path = require('path')
 const { Authflow: PrismarineAuth, Titles } = require('prismarine-auth')
 const minecraftFolderPath = require('minecraft-folder-path')
 const debug = require('debug')('minecraft-protocol')
+const { RealmAPI } = require('prismarine-realms')
 
-async function authenticate (client, options) {
+function validateOptions (options) {
   if (!options.profilesFolder) {
     options.profilesFolder = path.join(minecraftFolderPath, 'nmp-cache')
   }
-
   if (options.authTitle === undefined) {
     options.authTitle = Titles.MinecraftNintendoSwitch
     options.deviceType = 'Nintendo'
     options.flow = 'live'
   }
+}
 
   client.authflow = new PrismarineAuth(options.username, options.profilesFolder, options, options.onMsaCode)
   const { token, entitlements, profile, certificates } = await client.authflow.getMinecraftJavaToken({ fetchProfile: true, fetchCertificates: !options.disableChatSigning }).catch(e => {
@@ -42,6 +43,38 @@ async function authenticate (client, options) {
   options.connect(client)
 }
 
+async function realmAuthenticate (options) {
+  validateOptions(options)
+
+  options.authflow = new PrismarineAuth(options.username, options.profilesFolder, options, options.onMsaCode)
+
+  const api = RealmAPI.from(options.authflow, 'java')
+  const realms = await api.getRealms()
+
+  debug('realms', realms)
+
+  if (!realms || !realms.length) throw Error('Couldn\'t find any Realms for the authenticated account')
+
+  let realm
+
+  if (options.realms.realmId) {
+    realm = realms.find(e => e.id === Number(options.realms.realmId))
+  } else if (options.realms.pickRealm) {
+    if (typeof options.realms.pickRealm !== 'function') throw Error('realms.pickRealm must be a function')
+    realm = await options.realms.pickRealm(realms)
+  }
+
+  if (!realm) throw Error('Couldn\'t find a Realm to connect to. Authenticated account must be the owner or has been invited to the Realm.')
+
+  const { host, port } = await realm.getAddress()
+
+  debug('realms connection', { host, port })
+
+  options.host = host
+  options.port = port
+}
+
 module.exports = {
-  authenticate
+  authenticate,
+  realmAuthenticate
 }
