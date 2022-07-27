e4ad28a55a4199be797072a7d3034dff1f3ee763
Jordan Jones <jordpjones@gmail.com>
2022-07-27 11:39:02 -0700

    Handle auth potentially being an function

--- src/createClient.js
+++ src/createClient.js
@@ -34,6 +34,9 @@ function createClient (options) {
   const client = new Client(false, version.minecraftVersion, options.customPackets, hideErrors)
 
   tcpDns(client, options)
+  if (options.auth instanceof Function) {
+      options.auth(client, options)
+  } else {
   switch (options.auth) {
     case 'mojang':
       console.warn('[deprecated] mojang auth servers no longer accept mojang accounts to login. convert your account.\nhttps://help.minecraft.net/hc/en-us/articles/4403181904525-How-to-Migrate-Your-Mojang-Account-to-a-Microsoft-Account')
@@ -47,6 +50,7 @@ function createClient (options) {
       client.username = options.username
       options.connect(client)
       break
+    }
   }
   if (options.version === false) autoVersion(client, options)
   setProtocol(client, options)
