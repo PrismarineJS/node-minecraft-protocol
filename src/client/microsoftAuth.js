15c6ac83e01cdb0ac1dc1bc012ba2cbe646be17c
unknown <lucienharryholloway@hotmail.co.uk>
2023-01-15 15:19:35 +0000

    Fix overwriting existing authflow

--- src/client/microsoftAuth.js
+++ src/client/microsoftAuth.js
@@ -18,7 +18,7 @@ function validateOptions (options) {
 async function authenticate (client, options) {
   validateOptions(options)
 
-  client.authflow = new PrismarineAuth(options.username, options.profilesFolder, options, options.onMsaCode)
+  client.authflow ??= new PrismarineAuth(options.username, options.profilesFolder, options, options.onMsaCode)
   const { token, entitlements, profile, certificates } = await client.authflow.getMinecraftJavaToken({ fetchProfile: true, fetchCertificates: !options.disableChatSigning }).catch(e => {
     if (options.password) console.warn('Sign in failed, try removing the password field\n')
     if (e.toString().includes('Not Found')) console.warn(`Please verify that the account ${options.username} owns Minecraft\n`)
