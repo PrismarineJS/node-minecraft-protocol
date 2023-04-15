c8acc5bfaed8c9ebb0b8f99ff37abfcf5db9ed84
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-04-15 16:12:51 +0200

    Create multiprotocol.js

---
+++ examples/server_multiprotocol/multiprotocol.js
@@ -0,0 +1,16 @@
+// made for 1.19 - 1.19.3
+
+const server = require('minecraft-protocol').createServer({
+    encryption: false,
+    version: false,
+    motd: 'Multiprotocol example',
+    'online-mode': false
+});
+
+server.on('login', function (client) {
+    console.info(client.username + ' joined!')
+
+    const mcData = require('minecraft-data')(server.version)
+
+    client.write('login', mcData.loginPacket);
+});
