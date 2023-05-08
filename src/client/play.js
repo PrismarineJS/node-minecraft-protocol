8b07304552d8d0eb9471d0d3d607b1d97c762037
XHawk87 <hawk87@hotmail.co.uk>
2023-05-08 20:37:33 +0100

    Typo in "cypher" property check. Fixes #1106

--- src/client/play.js
+++ src/client/play.js
@@ -13,7 +13,7 @@ module.exports = function (client, options) {
 
   client.once('login', () => {
     const mcData = require('minecraft-data')(client.version)
-    if (mcData.supportFeature('useChatSessions') && client.profileKeys && client._cipher) {
+    if (mcData.supportFeature('useChatSessions') && client.profileKeys && client.cipher) {
       client._session = {
         index: 0,
         uuid: uuid.v4fast()
