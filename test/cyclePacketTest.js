6ea193b894525a2d3314f09bf3bcb1ae4a45ed24
Romain Beaumont <romain.rom1@gmail.com>
2023-12-27 17:28:21 +0100

    fix cycle packet test

--- test/cyclePacketTest.js
+++ test/cyclePacketTest.js
@@ -8,7 +8,7 @@ const makeClientSerializer = version => createSerializer({ state: states.PLAY, v
 const makeClientDeserializer = version => createDeserializer({ state: states.PLAY, version })
 
 for (const supportedVersion of supportedVersions) {
-  let serializer, deserializer
+  let serializer, deserializer, data
   const mcData = require('minecraft-data')(supportedVersion)
   const version = mcData.version
 
@@ -27,20 +27,27 @@ for (const supportedVersion of supportedVersions) {
     assert.strictEqual(areEq, true, `Error when testing ${+packetIx + 1} ${packetName} packet`)
   }
   describe(`Test cycle packet for version ${version.minecraftVersion}`, () => {
-    serializer = makeClientSerializer(version.minecraftVersion)
-    deserializer = makeClientDeserializer(version.minecraftVersion)
-    if (mcPackets.pc[version.minecraftVersion] === undefined) {
-      console.log(`Version ${version.minecraftVersion} has no packet dump.`)
-      return
-    }
-    const data = mcPackets.pc[version.minecraftVersion]
-    // server -> client
-    Object.entries(data['from-server']).forEach(([packetName, packetData]) => {
-      it(`${packetName} packet`, () => {
-        for (const i in packetData) {
-          testBuffer(packetData[i].raw, [packetName, i])
-        }
-      })
+    before(() => {
+      serializer = makeClientSerializer(version.minecraftVersion)
+      deserializer = makeClientDeserializer(version.minecraftVersion)
     })
+    data = mcPackets.pc[version.minecraftVersion]
+    it('Has packet data', () => {
+      if (data === undefined) {
+        // many version do not have data, so print a log for now
+        // assert when most versions have packet data
+        console.log(`Version ${version.minecraftVersion} has no packet dump.`)
+      }
+    })
+    // server -> client
+    if (data !== undefined) {
+      Object.entries(data['from-server']).forEach(([packetName, packetData]) => {
+        it(`${packetName} packet`, () => {
+          for (const i in packetData) {
+            testBuffer(packetData[i].raw, [packetName, i])
+          }
+        })
+      })
+    }
   })
 }
